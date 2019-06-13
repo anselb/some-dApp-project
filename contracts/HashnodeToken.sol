c93a06aef60f2b9c01ada5bf4cfe55dcdbe23c00
Ansel Bridgewater <iamansel@gmail.com>
2019-06-13 16:41:24 -0700

    Initialized truffle project and added token

---
+++ contracts/HashnodeToken.sol
@@ -0,0 +1,9 @@
+pragma solidity ^0.4.18;
+
+import 'openzeppelin-solidity/contracts/token/ERC20/ERC20Mintable.sol';
+
+contract HashnodeToken is ERC20Mintable {
+  string public name = "Hashnode Token";
+  string public symbol = "HT";
+  uint8 public decimals = 18;
+}
